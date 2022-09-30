9ecae44327f9e66b6a5cab01d7dfd77a0291bf0b
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-09-30 13:51:45 -0700

    commit tree ba4050905cfa52b5b1d00a0520345ac7bddfea7f

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,10 @@
+# app/models/movie.rb
 class Movie < ApplicationRecord
   belongs_to :director
   has_many :reviews
+end
+
+# app/models/review.rb
+class Review < ApplicationRecord
+  belongs_to :movie
 end
